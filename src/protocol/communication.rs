6e94ef118c0ea0f380205f55a0a8ae2ab45e2b86
Neil Tallim <neiltallim@3d-p.com>
2021-04-01 11:49:05 -0600

    Documentation logging

--- src/protocol/communication.rs
+++ src/protocol/communication.rs
@@ -1,29 +1,31 @@
-extern crate log;
-
-use std::error::Error;
-use std::io;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write};
 use std::time::Duration;
 
-use mio::net::{TcpStream};
 use mio::{Events, Ready, Poll, PollOpt, Token};
+use mio::net::{TcpStream};
 
+use std::error::Error;
 type BoxResult<T> = Result<T,Box<dyn Error>>;
 
-pub const KEEPALIVE_DURATION:Duration = Duration::from_secs(2); //communication over the protocol link will normally happen once per second
+/// how long to wait for keepalive events
+// the communications channels typically exchange data every second, so 2s is reasonable to avoid excess noise
+pub const KEEPALIVE_DURATION:Duration = Duration::from_secs(2);
 
+/// how long to block on polling operations
 const POLL_TIMEOUT:Duration = Duration::from_millis(50);
 
-
+/// sends JSON data over a client-server communications stream
 pub fn send(stream:&mut TcpStream, message:&serde_json::Value) -> BoxResult<()> {
     let serialised_message = serde_json::to_vec(message)?;
     
+    log::debug!("sending message of length {} to {}", serialised_message.len(), stream.peer_addr()?);
     let mut output_buffer = vec![0_u8; (serialised_message.len() + 2).into()];
     output_buffer[..2].copy_from_slice(&(serialised_message.len() as u16).to_be_bytes());
     output_buffer[2..].copy_from_slice(serialised_message.as_slice());
     Ok(stream.write_all(&output_buffer)?)
 }
 
+/// receives the length-count of a pending message over a client-server communications stream
 fn receive_length(stream:&mut TcpStream, alive_check:fn() -> bool, results_handler:&mut dyn FnMut() -> BoxResult<()>) -> BoxResult<u16> {
     let mut cloned_stream = stream.try_clone()?;
     
@@ -57,8 +59,11 @@ fn receive_length(stream:&mut TcpStream, alive_check:fn() -> bool, results_handl
                             
                             length_bytes_read += size;
                             if length_bytes_read == 2 {
-                                //poll.deregister(cloned_stream)?;
-                                return Ok(u16::from_be_bytes(length_spec));
+                                let length = u16::from_be_bytes(length_spec);
+                                log::debug!("received length-spec of {} from {}", length, stream.peer_addr()?);
+                                return Ok(length);
+                            } else {
+                                log::debug!("received partial length-spec from {}", stream.peer_addr()?);
                             }
                         },
                         Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => { //nothing left to process
@@ -74,6 +79,7 @@ fn receive_length(stream:&mut TcpStream, alive_check:fn() -> bool, results_handl
     }
     Err(Box::new(simple_error::simple_error!("system shutting down")))
 }
+/// receives the data-value of a pending message over a client-server communications stream
 fn receive_payload(stream:&mut TcpStream, alive_check:fn() -> bool, results_handler:&mut dyn FnMut() -> BoxResult<()>, length:u16) -> BoxResult<serde_json::Value> {
     let mut cloned_stream = stream.try_clone()?;
     
@@ -109,13 +115,15 @@ fn receive_payload(stream:&mut TcpStream, alive_check:fn() -> bool, results_hand
                             if bytes_read == length as usize {
                                 match serde_json::from_slice(&buffer) {
                                     Ok(v) => {
-                                        //poll.deregister(cloned_stream)?;
+                                        log::debug!("received {:?} from {}", v, stream.peer_addr()?);
                                         return Ok(v);
                                     },
                                     Err(e) => {
                                         return Err(Box::new(e));
                                     },
                                 }
+                            } else {
+                                log::debug!("received partial payload from {}", stream.peer_addr()?);
                             }
                         },
                         Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => { //nothing left to process
@@ -131,7 +139,10 @@ fn receive_payload(stream:&mut TcpStream, alive_check:fn() -> bool, results_hand
     }
     Err(Box::new(simple_error::simple_error!("system shutting down")))
 }
+/// handles the full process of retrieving a message from a client-server communications stream
 pub fn receive(mut stream:&mut TcpStream, alive_check:fn() -> bool, results_handler:&mut dyn FnMut() -> BoxResult<()>) -> BoxResult<serde_json::Value> {
+    log::debug!("awaiting length-value from {}", stream.peer_addr()?);
     let length = receive_length(&mut stream, alive_check, results_handler)?;
+    log::debug!("awaiting payload from {}", stream.peer_addr()?);
     receive_payload(&mut stream, alive_check, results_handler, length)
 }
